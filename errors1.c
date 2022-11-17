09414f93754f970fff3dfddcf3377a01a402db97
Samydulait <Samydulait@github.com>
2022-11-17 09:32:14 -0800

    simple_shell

--- errors1.c
+++ errors1.c
@@ -129,10 +129,10 @@ char *convert_number(long int num, int base, int flags)
  */
 void remove_comments(char *buf)
 {
-	int i
+	int i;
 
 	for (i = 0; buf[i] != '\0'; i++)
-		if (buf[i] == '#' && (!i || buf[i -1] == ' '))
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
 			buf[i] = '\0';
 			break;
